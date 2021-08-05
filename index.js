96bad83982dc649897356b75fa5306ff9b993dfd
OksanaBakl <oksanamolesha@ukr.net>
2021-08-05 15:41:25 +0300

    add timer

---
+++ index.js
@@ -0,0 +1,59 @@
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  // intervalId = null;
+
+  start() {
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      const { days, hours, mins, secs } = this.getTimeComponents(deltaTime);
+
+      this.updateClockface({ days, hours, mins, secs });
+    }, 1000);
+  }
+
+  // рисуем интерфейс
+  updateClockface({ days, hours, mins, secs }) {
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.mins.textContent = `${mins}`;
+    refs.secs.textContent = `${secs}`;
+  }
+  // Для подсчета значений используем следующие готовые формулы,
+  // где time - разница между targetDate и текущей датой.
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  // Принимает число, приводит к строке и добавляет в начало 0 если число меньше 2-х знаков
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+// Настройки нового таймера
+const countdownTimer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Sep 17, 2021'),
+});
+
+countdownTimer.start();
